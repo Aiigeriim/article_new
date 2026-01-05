521b2625511331794438e579e0989542bdc80246
Aiigeriim <sardarbekova1005@gmail.com>
2026-01-05 16:55:35 +0300

    дз № 68 like/unlike стаей и комментариев доведен до ума

--- source/webapp/static/js/like-script.js
+++ source/webapp/static/js/like-script.js
@@ -1,5 +1,3 @@
-console.log('test')
-
 async function makeRequest(url, method = 'GET') {
     let response = await fetch(url, {"method": method})
     if (response.ok) {
@@ -12,30 +10,26 @@ async function makeRequest(url, method = 'GET') {
 
 async function onClick(event) {
     event.preventDefault();
-    console.log(event.target);
     let btn = event.target;
     let url = btn.href;
     let response = await makeRequest(url);
-
     if (response.action === 'Liked') {
         btn.innerText = 'Unlike';
     } else {
         btn.innerText = 'Like';
     }
 
-    let counter = document.getElementById(`article-${btn.dataset.articleId}`)
+    let counter = btn.parentElement.getElementsByTagName("span")[0];
     counter.innerText = response.likes_count
-
 
 }
 
 function onLoad() {
-    let likeButtons = document.querySelectorAll('[data-like="article"]');
+    let likeButtons = document.querySelectorAll('[data-like="like"]');
     for (let likeBtn of likeButtons) {
         likeBtn.addEventListener("click", onClick);
-        console.log(likeBtn);
     }
 
 }
 
-window.addEventListener("load", onLoad)
+window.addEventListener("load", onLoad);
